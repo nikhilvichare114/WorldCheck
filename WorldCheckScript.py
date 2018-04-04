6d78688804a48f4cf5eca9df2bd88970c5b9ad07
Vichare <vichareN@emea.travelex.net>
2018-04-04 15:06:46 +0530

    Adding function 'getMD5Checksum()' to get MD5 for a file and pass it to the function which downloads file and puts it into S3 to maintain its integrity

--- WorldCheckScript.py
+++ WorldCheckScript.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 from __future__ import print_function
 import boto3
 import urllib2
@@ -24,7 +25,7 @@ class HashMD5Exception(Exception):
 
 
 def worldCheckFileLoad():
-    
+
     url = os.environ['url']
     username = os.environ['username']
     password = os.environ['password']
@@ -35,33 +36,31 @@ def worldCheckFileLoad():
     urlHash = hashlib.md5()
     dloadHash = hashlib.md5()
     try:
+        md5 = getMD5Chcksum(username, password)
+        hex_string,md5FileName=md5.split("  ")
         result = urllib2.urlopen(request)
-        
+
         if result.code == 200:
-            
+
             output = cStringIO.StringIO()
             output.write(result.read())
-            hex_string='c4a89d66879af372f05d160fefaabbdd'
-            encoded = hex_string.decode("hex")
-            print(encoded.encode("base64"))
+            decoded = hex_string.strip().decode('hex')
             bucketName = os.environ['bucketName']
             s3_config = Config(signature_version='s3v4')
             s3 = boto3.client('s3', config=s3_config)
-            SSEKMSKeyId=os.environ['SSEKMSKeyId']
+            SSEKMSKeyId = os.environ['SSEKMSKeyId']
+            encodedBase64=base64.b64encode(decoded)
             s3.put_object(
                 Key=fileName,
                 Body=output.getvalue(),
                 Bucket=bucketName,
-                ContentMD5=encoded.encode("base64"),
+                ContentMD5=encodedBase64,
                 SSEKMSKeyId=SSEKMSKeyId,
                 ServerSideEncryption='aws:kms',
                 ACL='private',
-                Metadata={
-                    'md5chksum': 'lTCOfxkMe+PoPRgwdHEIWg=='
-                },
+                Metadata={'md5': hex_string,'base64': encodedBase64},
                 )
-            
-            
+
             print('File added to S3 bucket successfully!!')
         else:
             print('Error!!')
@@ -80,3 +79,23 @@ def worldCheckFileLoad():
 
         print('GenericError!!')
         checksLogger.error('generic exception = ' + str(e))
+
+
+def getMD5Chcksum(username, password):
+    try:
+        md5Url = \
+            'https://www.world-check.com/portal/Downloads/world-check-deleted-day-v2.xml.md5'
+        request = urllib2.Request(md5Url, headers={'accept': '*/*'})
+        base64string = base64.b64encode('%s:%s' % (username, password))
+        request.add_header('Authorization', 'Basic %s' % base64string)
+        result = urllib2.urlopen(request)
+    except urllib2.HTTPError, e:
+        print('HTTPError!!')
+        checksLogger.error('HTTPError = ' + str(e.code))
+    except urllib2.URLError, e:
+        print('URLError!!')
+        checksLogger.error('URLError = ' + str(e.reason))
+    except Exception, e:
+        print('GenericError!!')
+        checksLogger.error('generic exception = ' + str(e))
+    return result.read()
