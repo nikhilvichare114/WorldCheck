e685f8376788003109993f6093848c7689d0a373
Vichare <vichareN@emea.travelex.net>
2018-04-05 12:25:46 +0530

    Formatting code using pep8 and adding comments to the code

--- WorldCheckScript.py
+++ WorldCheckScript.py
@@ -23,6 +23,8 @@ class HashMD5Exception(Exception):
 
     pass
 
+# downloads the Worldcheckfile and saves it into S3 bucket.
+
 
 def worldCheckFileLoad():
 
@@ -36,8 +38,12 @@ def worldCheckFileLoad():
     urlHash = hashlib.md5()
     dloadHash = hashlib.md5()
     try:
+                # calling the md5 function to get the MD5 checksum of the file
+                # which will help us maintain the integrity of the file that we
+                # download and place into S3 bucket
         md5 = getMD5Chcksum(username, password)
-        hex_string,md5FileName=md5.split("  ")
+        hex_string, md5FileName = md5.split("  ")
+        # reads the data of Worldcheck file from the URL.
         result = urllib2.urlopen(request)
 
         if result.code == 200:
@@ -49,7 +55,8 @@ def worldCheckFileLoad():
             s3_config = Config(signature_version='s3v4')
             s3 = boto3.client('s3', config=s3_config)
             SSEKMSKeyId = os.environ['SSEKMSKeyId']
-            encodedBase64=base64.b64encode(decoded)
+            encodedBase64 = base64.b64encode(decoded)
+            # put_object() helps to put the file into S3 bucket.
             s3.put_object(
                 Key=fileName,
                 Body=output.getvalue(),
@@ -58,44 +65,48 @@ def worldCheckFileLoad():
                 SSEKMSKeyId=SSEKMSKeyId,
                 ServerSideEncryption='aws:kms',
                 ACL='private',
-                Metadata={'md5': hex_string,'base64': encodedBase64},
-                )
+                Metadata={'md5': hex_string, 'base64': encodedBase64},
+            )
 
             print('File added to S3 bucket successfully!!')
         else:
             print('Error!!')
-    except urllib2.HTTPError, e:
+    except urllib2.HTTPError as e:
 
         print('HTTPError!!')
         checksLogger.error('HTTPError = ' + str(e.code))
-    except urllib2.URLError, e:
+    except urllib2.URLError as e:
 
         print('URLError!!')
         checksLogger.error('URLError = ' + str(e.reason))
     except HashMD5Exception:
 
         print('ERROR = MD5 Exception occured !!')
-    except Exception, e:
+    except Exception as e:
 
         print('GenericError!!')
         checksLogger.error('generic exception = ' + str(e))
 
+# The Function needs username and password required to access url and
+# returns the data that is read from the file.
+
 
 def getMD5Chcksum(username, password):
     try:
-        md5Url = \
-            'https://www.world-check.com/portal/Downloads/world-check-deleted-day-v2.xml.md5'
+        md5Url = os.environ['md5Url']
         request = urllib2.Request(md5Url, headers={'accept': '*/*'})
         base64string = base64.b64encode('%s:%s' % (username, password))
         request.add_header('Authorization', 'Basic %s' % base64string)
+        # Reads the data from MD5 file which will be used as checksum for
+        # worldcheck file.
         result = urllib2.urlopen(request)
-    except urllib2.HTTPError, e:
+    except urllib2.HTTPError as e:
         print('HTTPError!!')
         checksLogger.error('HTTPError = ' + str(e.code))
-    except urllib2.URLError, e:
+    except urllib2.URLError as e:
         print('URLError!!')
         checksLogger.error('URLError = ' + str(e.reason))
-    except Exception, e:
+    except Exception as e:
         print('GenericError!!')
         checksLogger.error('generic exception = ' + str(e))
     return result.read()
